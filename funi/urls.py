44d0305850473567e12154a688e4095284a6ee0b
ramaminiaina <mamy@aims.ac.za>
2024-04-30 20:53:58 +0200

    add about app

--- funi/urls.py
+++ funi/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name='home'),
     path("shop/", include('shop.urls')),
+    path("about/", include('about.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
